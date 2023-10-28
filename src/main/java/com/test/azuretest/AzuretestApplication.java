622ecc658e20917b1dcfac528e502143ca63ca75
arvindconduent <arvind.sriram@conduent.com>
2023-10-28 00:48:56 -0400

    first commit

---
+++ src/main/java/com/test/azuretest/AzuretestApplication.java
@@ -0,0 +1,21 @@
+package com.test.azuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzuretestApplication {
+	
+	@GetMapping ("/message")
+	public String message() {
+	     return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuretestApplication.class, args);
+	}
+
+}
