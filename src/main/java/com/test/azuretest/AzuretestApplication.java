34355b4a6b6824172d00524e8b9cfbfef48f9a0f
arvindconduent <arvind.sriram@conduent.com>
2023-11-01 00:41:05 -0400

    message v1

--- src/main/java/com/test/azuretest/AzuretestApplication.java
+++ src/main/java/com/test/azuretest/AzuretestApplication.java
@@ -11,7 +11,7 @@ public class AzuretestApplication {
 	
 	@GetMapping ("/message")
 	public String message() {
-	     return "Hello world Git hub actions testing";
+	     return "Hello world Git hub actions testing v1";
 	}
 
 	public static void main(String[] args) {
